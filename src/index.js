9e9ad2323e05a58de391b02585f83038533d76d3
林昊 <2212477518@qq.com>
2021-03-05 14:11:21 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import * as React from "react";
+// import {Component} from "react";
+// import * as ReactDOM from "react-dom";
+
+import { React, ReactDOM } from './CONST';
+
+import './index.css';
+
+import App from './App';
+
+debugger;
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// console.log("React", React.version); //sy-log
+
+// 原生标签节点
+// 文本节点
+// 函数组件
+// 类组件
